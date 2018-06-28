acc93afb581395db41f912eb38d1858bfceb54b3
yqq <yqq@yqq.local>
2018-06-28 15:41:17 +0800

    Initial Commit

---
+++ Hello World/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by yqq on 2018/6/28.
+//  Copyright © 2018年 ybq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
